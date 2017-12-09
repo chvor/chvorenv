25b29c2aa2d99641e18ac6bf38e36221b0f74c80
viktor <viktor.ivanov@gmail.com>
2017-12-09 12:15:31 +0200

    Added ag to fzf

--- fish/config.fish
+++ fish/config.fish
@@ -10,4 +10,8 @@ if test -n "$TMUX" -a "x$TERM" = "xscreen" -a "x$ORIGTERM" = "xxterm-256color"
 	set -gx TERM screen-256color
 end
 
+if type -f -q fzf; and type -f -q ag
+	set -x FZF_DEFAULT_COMMAND 'ag --hidden -g ""'
+end
+
 . ~/.config/fish/solarized.fish
